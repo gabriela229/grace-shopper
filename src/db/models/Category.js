78ac61c7bf6e1dd3e5dfb73101fc868a24927ddf
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-10-04 20:34:22 -0400

    basic Category model

---
+++ src/db/models/Category.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Category = db.define('category', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = Category;
