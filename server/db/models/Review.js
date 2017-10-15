992817c0748049dae465833df2558ca500083c2f
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-10-14 23:11:19 -0400

    delete title attribute; description should be enough

--- server/db/models/Review.js
+++ server/db/models/Review.js
@@ -2,10 +2,6 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Review = db.define('review', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
   description: {
     type: Sequelize.STRING,
     allowNull: false
