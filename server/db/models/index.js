8028e5c054e58d178bdf6ab27946b171b1fc108f
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-10-14 23:08:58 -0400

    add review associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@ const Category = require('./Category');
 const Product = require('./Product');
 const LineItem = require('./LineItem');
 const Order = require('./Order');
+const Review = require('./Review');
 const User = require('./User');
 
 // ASSOCIATION(s)
@@ -11,11 +12,15 @@ LineItem.belongsTo(Product);
 LineItem.belongsTo(Order);
 Order.hasMany(LineItem);
 Order.belongsTo(User);
+Review.belongsTo(Product);
+Review.belongsTo(User, {as: 'reviewer'});
+Product.hasMany(Review);
 
 module.exports = {
   Category,
   Product,
   LineItem,
   Order,
+  Review,
   User
 };
