b7aac3f71bf2baf3bad6dc762ef2092cfa2fa05d
Barto Molina <bartomolina@gmail.com>
2017-10-21 14:00:44 -0400

    removed pg-parse-float 2

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize');
 
-console.log(require('pg').defaults)
-
 const db = new Sequelize(
     process.env.DATABASE_URL ||
     'postgres://localhost:5432/graceshopper', {
