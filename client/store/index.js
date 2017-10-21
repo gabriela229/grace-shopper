51836ecb4e4498ee8918342af01e939479ed1b55
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAs-MacBook-Pro.local>
2017-10-21 19:39:19 -0400

    merging master to my branch

--- client/store/index.js
+++ client/store/index.js
@@ -9,13 +9,10 @@ import products from './products';
 import authUser from './auth';
 import users from './users';
 import reviews from './reviews';
-<<<<<<< HEAD
 import checkout from './checkout';
-=======
 import {loadState} from './localStorage';
 
 const persistedState = loadState();
->>>>>>> ce258199cc27c43a1f070481db8a000e9dbf03ec
 
 const rootReducer = combineReducers({
   authUser,
@@ -24,12 +21,8 @@ const rootReducer = combineReducers({
   error,
   products,
   reviews,
-<<<<<<< HEAD
   users,
   checkout                          // an object
-=======
-  users
->>>>>>> ce258199cc27c43a1f070481db8a000e9dbf03ec
 });
 
 export default createStore(rootReducer, persistedState, applyMiddleware(thunkMiddleware, logger));
@@ -42,8 +35,6 @@ export * from './error';
 export * from './users';
 export * from './cart';
 export * from './reviews';
-<<<<<<< HEAD
 export * from './checkout';
-=======
 export * from './localStorage';
->>>>>>> ce258199cc27c43a1f070481db8a000e9dbf03ec
+
